1a1bf47c09c70e0c354b327a33200d25438cac70
vnovikov <nik.hbb@gmail.com>
2024-02-18 09:17:05 +0200

    Add file reader

--- src/FileReadService.java
+++ src/FileReadService.java
@@ -1,2 +1,18 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class FileReadService {
+    public void readFromFile(String filePath){
+        FileInputStream fin;
+
+        try {
+            fin = new FileInputStream(filePath);
+            int i;
+            while ((i = fin.read()) != -1) {
+                System.out.print((char) i);
+            }
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
 }
