28845aebd2700496e4f4aa9456d0e1c0338572dc
vnovikov <nik.hbb@gmail.com>
2024-02-18 09:16:28 +0200

    Add file writer

--- src/FileWriteService.java
+++ src/FileWriteService.java
@@ -1,2 +1,22 @@
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class FileWriteService {
+    public String writeToFile(String filePath, String text) {
+        FileOutputStream fos;
+
+        try {
+            fos = new FileOutputStream(filePath);
+            byte[] byteArr = text.getBytes();
+            fos.write(byteArr, 0, byteArr.length);
+            fos.close();
+            System.out.println("\n<< Текст записано. Шлях до файлу " + filePath);
+        } catch (IOException e) {
+            return e.getMessage();
+        }
+
+        return "Success!";
+    }
+
 }
